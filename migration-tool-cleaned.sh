bea9505df6ef05d6f3f76fc2a4ade3f3708497d9
Marcela Maslanova <marcela.maslanova@suse.com>
2024-11-22 14:48:40 +0100

    Adjust changes from get-o-o branch
    Leap -> Slowroll works
    Leap -> Tumbleweed works

--- migration-tool-cleaned.sh
+++ migration-tool-cleaned.sh
@@ -143,27 +143,101 @@ for key in "${!MIGRATION_OPTIONS[@]}"; do
     DIALOG_ITEMS+=("$key" "${MIGRATION_OPTIONS[$key]}")
 done
 
-# Display dialog and get choice
+# Get terminal dimensions
+read -r term_width term_height < <(stty size)
+
+# Calculate dialog box dimensions with padding
+border=2
+dialog_width=$((term_width - 2 * border))
+dialog_height=$((term_height - 2 * border))
+
+# Ensure minimum dimensions for dialog box
+dialog_width=$((dialog_width < 20 ? 20 : dialog_width))
+dialog_height=$((dialog_height < 10 ? 10 : dialog_height))
+
+# Display a dialog with calculated size
+dialog --title "Dynamic Sizing Example" \
+       --msgbox "This dialog adjusts to your terminal size." \
+       "$dialog_height" "$dialog_width"
+
+# Display the dialog menu
 CHOICE=$(dialog --clear \
     --title "System Migration" \
     --menu "Select the migration target:" \
-    20 60 10 \
+    $dialog_width $dialog_height 100 \
     "${DIALOG_ITEMS[@]}" \
     2>&1 >/dev/tty) || exit
 
-# Handle user choice
+zypper in snapper grub2-snapper-plugin
+
+rpmsave_repo() {
+for repo_file in \
+repo-backports-debug-update.repo repo-oss.repo repo-backports-update.repo \
+repo-sle-debug-update.repo repo-debug-non-oss.repo repo-sle-update.repo \
+repo-debug.repo repo-source.repo repo-debug-update.repo repo-update.repo \
+repo-debug-update-non-oss.repo repo-update-non-oss.repo repo-non-oss.repo \
+download.opensuse.org-oss.repo download.opensuse.org-non-oss.repo download.opensuse.org-tumbleweed.repo \
+repo-openh264.repo openSUSE-*.repo repo-main.repo; do
+  if [ -f /etc/zypp/repos.d/$repo_file ]; then
+    echo "Content of $repo_file will be newly managed by zypp-services."
+    echo "Storing old copy as /etc/zypp/repos.d/$repo_file.rpmsave"
+    mv /etc/zypp/repos.d/$repo_file /etc/zypp/repos.d/$repo_file.rpmsave
+  fi
+done
+}
+
+# Clear the screen and handle the user choice
 clear
 if [[ -n $CHOICE ]]; then
     echo "Selected option: ${MIGRATION_OPTIONS[$CHOICE]}"
     case "${MIGRATION_OPTIONS[$CHOICE]}" in
         *"SUSE Linux Enterprise"*)
+cat > /etc/os-release  << EOL
+NAME="SLES" 
+VERSION="15-SP6" 
+VERSION_ID="15.6"
+PRETTY_NAME="SUSE Linux Enterprise Server 15 SP6"
+ID="sles"
+ID_LIKE="suse"
+ANSI_COLOR="0;32" 
+CPE_NAME="cpe:/o:suse:sles:15:sp6"
+DOCUMENTATION_URL="https://documentation.suse.com/"
+EOL
+
             $DRYRUN echo "Upgrading to ${MIGRATION_OPTIONS[$CHOICE]}"
+            zypper in suseconnect-ng
+            cp SLES.prod /etc/products.d/
+            rm -r /etc/products.d/baseproduct
+            ln -s /etc/products.d/SLES.prod /etc/products.d/baseproduct
+            rpmsave_repo
+            rpm -e --nodeps openSUSE-release
+            suseconnect -e marcela.maslanova@suse.com -r INTERNAL-USE-ONLY-2746-dfc5
+            suseconnect -e  email -r code 
+            SUSEConnect -p PackageHub/15.6/x86_64
+            zypper dup --allow-vendor-change --force-resolution -y
             ;;
         "openSUSE Tumbleweed")
             $DRYRUN echo "Upgrading to ${MIGRATION_OPTIONS[$CHOICE]}"
+            zypper ar -f -c http://download.opensuse.org/tumbleweed/repo/oss to-tumbleweed
+            zypper in openSUSE-repos-Tumbleweed && rpm -e --nodeps openSUSE-repos-Slowroll
+            zypper dup --allow-vendor-change --force-resolution -y
+            for repo in openSUSE-Leap*.repo openSUSE\:Leap*.repo; do
+                if [ -f /etc/zypp/repos.d/$repo ]; then
+                    mv /etc/zypp/repos.d/$repo /etc/zypp/repos.d/$repo.rpmsave
+                fi
+            done
             ;;
         "openSUSE Tumbleweed-Slowroll")
             $DRYRUN echo "Migrating to ${MIGRATION_OPTIONS[$CHOICE]}"
+            zypper addrepo https://download.opensuse.org/slowroll/repo/oss/ leap-to-slowroll
+            shopt -s globstar && TMPSR=$(mktemp -d) && zypper --pkg-cache-dir=${TMPSR} download openSUSE-repos-Slowroll && \
+                zypper modifyrepo --all --disable && zypper install ${TMPSR}/**/openSUSE-repos-Slowroll*.rpm && zypper dist-upgrade
+            zypper dup --allow-vendor-change --force-resolution -y
+            for repo in openSUSE-Leap*.repo openSUSE\:Leap*.repo; do
+                if [ -f /etc/zypp/repos.d/$repo ]; then
+                    mv /etc/zypp/repos.d/$repo /etc/zypp/repos.d/$repo.rpmsave
+                fi
+            done
             ;;
         *"openSUSE Leap"*)
             $DRYRUN echo "Upgrading to ${MIGRATION_OPTIONS[$CHOICE]}"
